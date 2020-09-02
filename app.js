48359f5303348881b0e986b5f9c6ab86e4974721
hecklerjim <59577337+hecklerjim@users.noreply.github.com>
2020-09-02 14:19:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+window.addEventListener('scroll', function(e) {
+    const target = document.querySelector(".scroll-fade");
+
+    var scrolled = window.pageYOffset;
+    var rate = scrolled * 0.0008;
+    
+    target.style.opacity = ''+rate+'';
+    console.log(rate);
+})
